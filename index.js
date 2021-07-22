6684a17a48180ad75fdc8e648cbf9fb901c6d0ce
kushalpatel42 <86609266+kushalpatel42@users.noreply.github.com>
2021-07-22 11:35:30 -0400

    leave some message

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+// myaccount
+function check(form){
+    if (form.username.value == 'Xander2355' && form.psw.value == 'Blackops3') {
+      window.open('practicelog.html')
+    } else {
+      alert('This username or password is incorrect');
+    }
+  }
